2efabe662fdc10cbdab0a8858c87420476494f21
Justice-love <eddyxu1213@126.com>
2018-10-17 17:26:18 +0800

    https://leetcode-cn.com/problems/longest-common-prefix/

--- longestCommonPrefix2.py
+++ longestCommonPrefix2.py
@@ -4,13 +4,10 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        print sorted(strs)
-        print strs
         if not strs:
             return ''
         min_1=min(strs)
         max_1=max(strs)
-        print min_1
         for i,value in enumerate(min_1):
             if value!=max_1[i]:
                 return min_1[:i]
